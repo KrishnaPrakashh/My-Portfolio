cc1c78128cac69e1573c3e1f077d48d3c959e45f
Krishna Prakash <krishnaprakashalpy@gmail.com>
2026-07-17 21:23:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import Education from "./components/Education";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
-import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import CursorGlow from "./components/CursorGlow";
@@ -23,7 +22,6 @@ function App() {
       <Skills />
       <Projects />
       <Experience />
-      <Certifications />
       <Contact />
       <Footer />
     </>
